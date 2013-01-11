ca90bb4f265d8fff87157dd858f7174a24a321e2
Artem Sheremet <dot.doom@gmail.com>
2013-01-11 13:12:23 +0300

    motion-blur: prev frame screenshot over current
    Renders previous frame screenshot over current with transparency = 0.8

---
+++ motion-blur.lua
@@ -0,0 +1,4 @@
+-- Motion blur-like effect
+-- Gens Lua script by the members of Youtube Sonic TAS community <http://ystc.ru>
+
+gens.registerbefore(function() gui.image(gui.gdscreenshot(), 0.8) end);
